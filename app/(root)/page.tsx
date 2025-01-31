8861147bfb7f8970fdfa98e78a3f05a617b40dbd
TEJAS07080 <tejas07080@gmail.com>
2025-01-31 22:27:14 +0530

    added background

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,10 @@
+import  { BackgroundBoxesDemo } from "@/components/Background";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      This edit is done by shubham
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <BackgroundBoxesDemo />
     </div>
   );
 }
